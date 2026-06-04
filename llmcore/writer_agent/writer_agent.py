371fb192a5655606c4d09f52200e0646139d0b61
Mehul1611 <mehulsharma1116@gmail.com>
2026-06-04 06:12:49 +0530

    feat/updated readme file

--- llmcore/writer_agent/writer_agent.py
+++ llmcore/writer_agent/writer_agent.py
@@ -1,4 +1,3 @@
-import json
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
@@ -19,36 +18,40 @@ class WriterAgent:
             model=LLMConstants.WRITER_AGENT_MODEL,
             base_url=LLMConstants.OLLAMA_BASE_URL,
             temperature=LLMConstants.WRITER_AGENT_TEMP,
+            format='json',
         )
+
+    def _get_structured_model(self):
+        return self._get_model().with_structured_output(DischargeSummaryOutput)
 
     def _get_prompt_template(self) -> ChatPromptTemplate:
         return ChatPromptTemplate.from_messages(
             [
                 SystemTemplate(
                     prompt=PromptTemplate(
-                        input_variables=['schema'], 
-                        template=SYSTEM_PROMPT
-                        )
-                    ),
+                        input_variables=['schema'],
+                        template=SYSTEM_PROMPT,
+                    )
+                ),
                 HumanTemplate(
                     prompt=PromptTemplate(
-                        input_variables=['structured_plan'], 
-                        template=USER_PROMPT
-                        )
-                    ),
+                        input_variables=['structured_plan'],
+                        template=USER_PROMPT,
+                    )
+                ),
             ]
         )
 
     def _build_prompt(self, structured_plan: str):
         prompt_template = self._get_prompt_template()
         return prompt_template.invoke({
-            'schema': json.dumps(DischargeSummaryOutput.model_json_schema(), indent=2),
+            'schema': DischargeSummaryOutput.model_json_schema(),
             'structured_plan': structured_plan,
         })
 
     @retry_llm_call()
     def invoke_writer_agent(self, structured_plan: str) -> dict:
-        model = self._get_model()
+        model = self._get_structured_model()
         prompt = self._build_prompt(structured_plan)
         response = model.invoke(prompt)
-        return json.loads(response.content)
+        return response.model_dump()
